8433e98d9b3d0a37a2a86d67bbb6f06d1fe8586b
Klimov Sergey <sergey.klimov.97@yandex.ru>
2018-12-23 16:06:58 +0300

    lab4 final version

---
+++ read.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+f = open('/home/sergey/Downloads/uniprot_sprot.fasta', 'r')
+
+seqs = []
+
+current_title = ''
+current_seq = ''
+
+for line in f.readlines():
+    if (line.startswith('>') or line.startswith('~')) and current_seq != '':
+        seqs.append([
+            current_title,
+            current_seq
+        ])
+        current_seq = ''
+    if line.startswith('~'):
+        break
+    if line.startswith('>'):
+        current_title = line.split('|')[1].strip()
+    else:
+        current_seq += line.rstrip()
+
+f.close()
+with open('base.json','w+') as f:
+    import json
+    json.dump(seqs,f,ensure_ascii=False)
